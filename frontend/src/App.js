0a7f9ddc313c4fb802470b6960c56b80a1eb5d77
preet <preet.basson@gmail.com>
2021-02-18 10:53:36 +0000

    V0.01: Initial

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>I Am the Knight</p>
+        <p>We Live In A Society</p>
+        <p>We Live In A Society</p>
+        <p>Why So Serious?</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
